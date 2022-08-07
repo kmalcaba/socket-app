a98697968a6f7babc3c9d0f77f9ccb68a5225059
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-06 18:30:19 -0700

    Listen for and emit socket events

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import io from "socket.io-client";
+import { useEffect } from "react";
+
+const socket = io.connect("http://localhost:5000");
 
 function App() {
-  return <div className="App"></div>;
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "Hello " });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div className="App">
+      <input placeholder="Message..." />
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
+  );
 }
 
 export default App;
